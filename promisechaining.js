aaa219d77806ef73d24839430f10743b0cd08e3e
Rohan Samavedam <42583450+rohansamavedam@users.noreply.github.com>
2019-07-06 12:14:49 +0530

    modified with async await

--- promisechaining.js
+++ promisechaining.js
@@ -1,9 +1,21 @@
 require('./db/mongoose')
 const Task = require('./models/task')
 
-Task.findByIdAndDelete('5d1f27dd4cdd98e9fcfd61e9').then(() => {
-    return Task.countDocuments({ completed: false })
-}).then((count) => {
+// Task.findByIdAndDelete('5d1f27dd4cdd98e9fcfd61e9').then(() => {
+//     return Task.countDocuments({ completed: false })
+// }).then((count) => {
+//     console.log(count)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed:false })
+    return count
+}
+
+deleteTaskAndCount('5d1f54013e2b65eab2aa5cbb').then((count) => {
     console.log(count)
 }).catch((error) => {
     console.log(error)
